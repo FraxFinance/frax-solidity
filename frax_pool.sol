bd015b80b06091c73b747d3140d261ef25795073
JasonHuan <sino@g.ucla.edu>
2020-06-29 04:15:51 -0700

    fixed and finished frax_pool_tether mint1t1()

--- frax_pool.sol
+++ frax_pool.sol
@@ -1,3 +1,5 @@
+pragma solidity ^0.6.0;
+
 import "./SafeMath.sol";
 import "./fxs.sol";
 import "./frax.sol";
@@ -7,8 +9,8 @@ contract frax_pool_tether {
     address fxs_address;
     address frax_address;
     address pool_oracle;
-    FRAXShares FXS = FRAXShares(fxs_address); 
-    FRAXStablecoin FRAX = FRAXStablecoin(frax_address); 
+    FRAXShares FXS;
+    FRAXStablecoin FRAX;
     uint256 public pool_col_price;
     
         constructor(
@@ -29,25 +31,31 @@ contract frax_pool_tether {
         pool_oracle = new_oracle;
     }
     
+    function setCollateralAddress(address collateral_address) public onlyByOracle {
+        collateral_token = ERC20(collateral_address);
+    }
+    
+    function setFRAXAddress(address frax_contract_address) public onlyByOracle {
+        FRAX = FRAXStablecoin(frax_contract_address);
+    }
+    
+    
     //these functions need modifiers to close them if phase 1 is over 
     
     //one way to shut off 1t1 minting is for oracles on the Frax contract to remove pool address from the mapping 
-    function mintFrax1t1(uint256 collateral_amount) public  {
+    function mintFrax1t1(uint256 collateral_amount) public payable {
     //first we must check if the collateral_ratio is  at 100%, if it is not, 1t1 minting is not active
     
-   collateral_token.transferFrom(msg.sender, address(this), collateral_amount); 
-    FRAX.pool_mint(tx.origin, collateral_amount); //then mints 1:1 to the caller and increases total supply 
-    
-    
+    collateral_token.transferFrom(msg.sender, address(this), collateral_amount); 
+        FRAX.pool_mint(tx.origin, collateral_amount); //then mints 1:1 to the caller and increases total supply 
     }
-    
     
     
     function redeem1t1(uint256 frax_amount) public  {
         
         //collaer must allow contract to burn frax from their balance first
         //why does _burn not work? 
-        FRAX._burn(tx.origin, frax_amount);
+        FRAX.burn(frax_amount);
 
         //sends tether back to the frax holder 1t1 after burning the frax
        collateral_token.transfer(tx.origin, frax_amount); 
