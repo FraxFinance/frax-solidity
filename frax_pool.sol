0ad962ebff5b60da6af740153eca6371096994a4
JasonHuan <sino@g.ucla.edu>
2020-07-06 21:44:52 -0700

    added in 6-decimal integer precision for collateral price; fixed issue in mintFrax()

--- frax_pool.sol
+++ frax_pool.sol
@@ -13,8 +13,7 @@ contract frax_pool {
     address pool_oracle;
     FRAXShares FXS;
     FRAXStablecoin FRAX;
-    uint256 collateral_price; 
-    uint256 public pool_col_price;
+    uint256 collateral_price_int; //6 decimals of precision, e.g. 1023000 represents $1.023
     //pool_ceiling is the total amount of collateral that a pool contract can hold
     uint256 pool_ceiling;
     
@@ -29,6 +28,10 @@ contract frax_pool {
     modifier onlyByOracle() {
         require(msg.sender == pool_oracle, "you're not the oracle :p");
         _;
+    }
+    
+    function _collateral_price() internal returns (uint256) { //retrieve price of collateral, in dollars
+        return collateral_price_int / 100000; //replace with safemath .div()
     }
     
     function setPoolCeiling(uint256 new_ceiling) public onlyByOracle {
@@ -48,19 +51,27 @@ contract frax_pool {
     }
     
     function setPrice(uint256 c_price) public onlyByOracle {
-        collateral_price = c_price;
+        collateral_price_int = c_price;
 
     }
     
     
     function mintFrax(uint256 collateral_amount, uint256 FXS_amount) public payable {
         
-        uint256 c_amount = (collateral_amount * collateral_price) / FRAX.global_collateral_ratio();
-        uint256 f_amount = (FXS_amount * FRAX.FXS_price()) / (1 - FRAX.global_collateral_ratio());
+        uint256 c_amount = (collateral_amount * _collateral_price()) / FRAX.global_collateral_ratio();//replace with safemath .div()
+        uint256 fxs_needed;
+        
+        if(FRAX.global_collateral_ratio() == 1){ //need to check here or else calculating f_amount throws dividebyzero error
+            collateral_token.transferFrom(msg.sender, address(this), collateral_amount);
+            FRAX.pool_mint(msg.sender, collateral_amount);
+            return;
+        }
+        
+        uint256 f_amount = (FXS_amount * FRAX.FXS_price()) / (1 - FRAX.global_collateral_ratio());//replace with safemath .div()
         
         if (c_amount < f_amount) {
             collateral_token.transferFrom(msg.sender, address(this), collateral_amount);
-            uint256 fxs_needed = c_amount * (1-FRAX.global_collateral_ratio());
+            fxs_needed = c_amount * (1-FRAX.global_collateral_ratio());
             FXS.transferFrom(msg.sender, address(frax_pool_address), fxs_needed);
             FRAX.pool_mint(msg.sender, collateral_amount);
             FXS.burn(fxs_needed);
