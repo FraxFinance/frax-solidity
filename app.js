c3c33ac29b8acdb8c2bc6810f3a15a349006b78c
JasonHuan <sino@g.ucla.edu>
2020-07-19 15:16:46 -0700

    added infura web3 provider

--- app.js
+++ app.js
@@ -1,54 +1,25 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/plain');
-	res.end('Hello World');
-});
-
-server.listen(port, hostname, () => {
-	console.log(`Server running at http://${hostname}:${port}/`);
-});
-
+//using infura provider on ropsten, currently access does not require whitelist (open to public)
 var Web3 = require('web3');
-var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
-console.log(`Provider at: ${web3.givenProvider}`);
-
-web3.eth.getAccounts(console.log);
+var web3 = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/v3/0a5b1633380b415d9b7342823baad798"))
 
 const solc = require('solc');
 const fs = require ('fs');
 const path = require('path');
 
-const contextPath = path.resolve(__dirname, 'Context.sol');
-const context = fs.readFileSync(contextPath, 'UTF-8');
-
-const ierc20Path = path.resolve(__dirname, 'IERC20.sol');
-const ierc20 = fs.readFileSync(ierc20Path, 'UTF-8');
-
-const safemathPath = path.resolve(__dirname, 'SafeMath.sol');
-const safemath = fs.readFileSync(safemathPath, 'UTF-8');
-
-const tethersamplePath = path.resolve(__dirname, 'tether_sample.sol');
-const tethersample = fs.readFileSync(tethersamplePath, 'UTF-8');
-
 var input = {
 	language: 'Solidity',
 	sources: {
 		'Context.sol': {
-			content: context
+			content: fs.readFileSync(path.resolve(__dirname, 'Context.sol'), 'UTF-8')
 		},
 		'IERC20.sol': {
-			content: ierc20
+			content: fs.readFileSync(path.resolve(__dirname, 'IERC20.sol'), 'UTF-8')
 		},
 		'SafeMath.sol': {
-			content: safemath
+			content: fs.readFileSync(path.resolve(__dirname, 'SafeMath.sol'), 'UTF-8')
 		},
 		'tether_sample.sol': {
-			content: tethersample
+			content: fs.readFileSync(path.resolve(__dirname, 'tether_sample.sol'), 'UTF-8')
 		},
 		'frax.sol': {
 			content: fs.readFileSync(path.resolve(__dirname, 'frax.sol'), 'UTF-8')
@@ -71,8 +42,53 @@ var input = {
 
 var output = JSON.parse(solc.compile(JSON.stringify(input)));
 
+
+/* print compiled contracts of specific .sol file
 for (var contractName in output.contracts['frax.sol']) {
 	console.log(contractName + ': ' + output.contracts['frax.sol'][contractName].evm.bytecode.object);
 }
+*/
+
+//setting contract provider - is this necessary while web3 provider is already set to same address?
+var Contract = require('web3-eth-contract');
+Contract.setProvider('https://ropsten.infura.io/v3/0a5b1633380b415d9b7342823baad798');
+
+//link metamask - this should be put into the front-end
+const ethEnabled = () => {
+  if (window.ethereum) {
+    window.web3 = new Web3(window.ethereum);
+    window.ethereum.enable();
+    return true;
+  }
+  return false;
+}
 
 
+
+//creating accounts
+var deploy_account = web3.eth.accounts.create();
+var deploy_account_address = deploy_account.address;
+var deploy_account_privkey = deploy_account.privateKey;
+var deploy_account_signTxObject = deploy_account.signTransaction;
+
+console.log('address: ' + deploy_account_address + '\nprivkey: ' + deploy_account_privkey);
+
+/* returns local accounts in web3 provider
+var tetherAccount = web3.eth.getAccounts().then(e => {
+	console.log(e);
+});
+
+console.log(`account: ` + tetherAccount);
+*/
+
+
+
+var tether_sample = new Contract(output.contracts['tether_sample.sol']['tether'].abi);
+
+tether_sample.deploy({
+	data: output.contracts['tether_sample.sol']['tether'].evm.bytecode,
+	arguments: ['USDT', '10000', web3.eth.getAccounts().then(e => {
+		firstAccount = e[0];
+		console.log('A: ' + firstAccount)
+	})]
+});
