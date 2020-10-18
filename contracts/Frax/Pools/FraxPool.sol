f51a26d729598205aa4926bcec256c4f5b581b4b
jasonhuan <sino@g.ucla.edu>
2020-10-18 16:17:08 -0700

    change availableExcessCollatDV() to use frax_supply instead of frax_dollar_value

--- contracts/Frax/Pools/FraxPool.sol
+++ contracts/Frax/Pools/FraxPool.sol
@@ -137,9 +137,9 @@ contract FraxPool is AccessControl {
 
     function availableExcessCollatDV() public view returns (uint256) {
         (uint256 frax_price, , uint256 total_supply, uint256 global_collateral_ratio, uint256 global_collat_value, , ) = FRAX.frax_info();
-        uint256 total_FRAX_dollar_value_d18 = total_supply.mul(1e6).div(frax_price); 
+        //uint256 total_FRAX_dollar_value_d18 = total_supply.mul(1e6).div(frax_price); 
         if (global_collateral_ratio > 1e6) global_collateral_ratio = 1e6; // Handles an overcollateralized contract with CR > 1
-        uint256 required_collat_dollar_value_d18 = (total_FRAX_dollar_value_d18.mul(global_collateral_ratio)).div(1e6);
+        uint256 required_collat_dollar_value_d18 = (total_supply.mul(global_collateral_ratio)).div(1e6); //calculates collateral needed to back each 1 FRAX with $1 of collateral at current collat ratio
         if (global_collat_value > required_collat_dollar_value_d18) return global_collat_value.sub(required_collat_dollar_value_d18);
         else return 0;
     }
