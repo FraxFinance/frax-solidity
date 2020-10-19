6d267981ad6bdfe244e3f8266766132894181dd7
jasonhuan <sino@g.ucla.edu>
2020-10-19 16:52:05 -0700

    FXS-26: add public constant variable of collateral address to show the address being used for collateral

--- contracts/Frax/Pools/Pool_USDT.sol
+++ contracts/Frax/Pools/Pool_USDT.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.6.0 <0.7.0;
 import "./FraxPool.sol";
 
 contract Pool_USDT is FraxPool {
+    address public USDT_address;
     constructor(
         address _collateral_address,
         address _oracle_address,
@@ -14,5 +15,6 @@ contract Pool_USDT is FraxPool {
     FraxPool(_collateral_address, _oracle_address, _creator_address, _timelock_address, _pool_ceiling)
     public {
     	_setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+        USDT_address = _collateral_address;
     }
 }
