75dd58ccbc3213919f4966307192b93b3eb5c935
jasonhuan <sino@g.ucla.edu>
2020-11-20 22:02:52 -0800

    DAI contracts

---
+++ contracts/Frax/Pools/Pool_DAI.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.11;
+
+import "./FraxPool.sol";
+
+contract Pool_DAI is FraxPool {
+    address public DAI_address;
+    constructor(
+        address _frax_contract_address,
+        address _fxs_contract_address,
+        address _collateral_address,
+        address _creator_address,
+        address _timelock_address,
+        uint256 _pool_ceiling
+    ) 
+    FraxPool(_frax_contract_address, _fxs_contract_address, _collateral_address, _creator_address, _timelock_address, _pool_ceiling)
+    public {
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+        DAI_address = _collateral_address;
+    }
+}
