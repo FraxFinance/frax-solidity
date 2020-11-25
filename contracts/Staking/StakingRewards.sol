39f4d48a5dd5ea26a025541adac76bba13a4b1db
jasonhuan <sino@g.ucla.edu>
2020-11-25 15:30:42 -0800

    require statement typo

--- contracts/Staking/StakingRewards.sol
+++ contracts/Staking/StakingRewards.sol
@@ -387,11 +387,12 @@ contract StakingRewards is IStakingRewards, RewardsDistributionRecipient, Reentr
 
     function setLockedStakeTimeForMinAndMaxMultiplier(uint256 _locked_stake_time_for_max_multiplier, uint256 _locked_stake_min_time) external onlyByOwnerOrGovernance {
         require(_locked_stake_time_for_max_multiplier >= 1, "Multiplier Max Time must be greater than or equal to 1");
-        require(_locked_stake_min_time >= 1, "Multiplier Max Time must be greater than or equal to 1");
+        require(_locked_stake_min_time >= 1, "Multiplier Min Time must be greater than or equal to 1");
+        
+        locked_stake_time_for_max_multiplier = _locked_stake_time_for_max_multiplier;
         
         locked_stake_min_time = _locked_stake_min_time;
         locked_stake_min_time_str = StringHelpers.uint2str(_locked_stake_min_time);
-        locked_stake_time_for_max_multiplier = _locked_stake_time_for_max_multiplier;
 
         emit LockedStakeMinTime(_locked_stake_min_time);
         emit LockedStakeTimeForMaxMultiplier(locked_stake_time_for_max_multiplier);
