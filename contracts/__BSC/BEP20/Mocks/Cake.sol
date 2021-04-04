39a9a557ce70fa73ba6b7e8636e3de3f500c8560
Travis <travis@everipedia.com>
2021-04-03 18:00:14 -0700

    BSC stuff

---
+++ contracts/__BSC/BEP20/Mocks/Cake.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.2;
+pragma experimental ABIEncoderV2;
+import "../BEP20.sol";
+
+// Mock Farm token
+contract Cake is BEP20("PancakeSwap Token", "CAKE") {
+
+}
---
+++ contracts/__BSC/BEP20/Mocks/FxsMock.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.2;
+pragma experimental ABIEncoderV2;
+import "../BEP20.sol";
+
+// Mock Farm token
+contract FxsMock is BEP20("Fxs", "FXS") {
+
+}
