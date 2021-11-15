a73236abb6810050fb01003081df07f7c793bf95
denett <35074672+denett@users.noreply.github.com>
2021-11-15 20:21:43 +0100

    Add files via upload

---
+++ src/hardhat/contracts/LeveragePool/TestERC20.sol
@@ -0,0 +1,17 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract TestERC20 is ERC20, ERC20Burnable, Ownable {
+    constructor() ERC20("TestERC20", "TestERC20") {}
+
+    function mint(address to, uint256 amount) external onlyOwner {
+      _mint(to, amount);
+    }
+}
