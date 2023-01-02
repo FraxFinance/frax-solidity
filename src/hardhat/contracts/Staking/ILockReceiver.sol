497f71ebac1c8d8df4333f8b86dc19f447cc97fd
ZrowGz <80988768+ZrowGz@users.noreply.github.com>
2023-01-02 09:28:34 -0900

    basic transferLocked erc20 (#165)
    * basic transferLocked erc20
    
    * transferred stakes get new kek_id
    
    * remove change
    
    * add in transferFrom functions, vars, and management; update require error strings to reduce contract size
    
    * update permissions checks
    
    * clean up comments
    
    * add approval view function
    
    * add onLockReceived functionality
    
    * onLockReceived logic
    
    * include beforeTransfer hook & option for destination kek_id selection
    
    * run reward&balance mdf for both receiver & sender
    
    * added foundry, updated new vs specified target stake to include better checks
    
    * corrections & separate allowance check from reduction. TODO: check double modifier usage & whether the lockedStake for user needs to have the check for if the liquidity is 0 already (kek is deleted when withdrawn & checks if it exists when _getStake is called...
    
    * corrections & separate allowance check from reduction. TODO: check double modifier usage & whether the lockedStake for user needs to have the check for if the liquidity is 0 already (kek is deleted when withdrawn & checks if it exists when _getStake is called...
    
    * compiler config settings
    
    * compiler config settings
    
    * updated from testing
    
    * removed foundry - testing performed in separate repository: zrowgz/frax-transfers
    
    * ILockReceiver added

--- src/hardhat/contracts/Staking/ILockReceiver.sol
+++ src/hardhat/contracts/Staking/ILockReceiver.sol
@@ -5,14 +5,22 @@ interface ILockReceiver {
     function beforeLockTransfer(
         address _sender,
         address _receiver,
+<<<<<<< HEAD
         uint256 _lockId,
+=======
+        bytes32 _lockId,
+>>>>>>> 93e23e7 (basic transferLocked erc20 (#165))
         bytes calldata _data
     ) external returns (bytes4);
 
     function onLockReceived(
         address _sender,
         address _receiver,
+<<<<<<< HEAD
         uint256 _lockId,
+=======
+        bytes32 _lockId,
+>>>>>>> 93e23e7 (basic transferLocked erc20 (#165))
         bytes calldata _data
     ) external returns (bytes4);
 }
