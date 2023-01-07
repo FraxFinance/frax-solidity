ff63307c81dec2142d8cdee660a4be4be2a189e3
Travis Moore <33413876+FortisFortuna@users.noreply.github.com>
2023-01-06 17:35:09 -0800

    Travis (#181)
    * prep for crosschain frxETH
    
    * minor fix

---
+++ src/hardhat/contracts/ERC20/__CROSSCHAIN/CrossChainCanonicalV2.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity >=0.8.0;
+
+// ====================================================================
+// |     ______                   _______                             |
+// |    / _____________ __  __   / ____(_____  ____ _____  ________   |
+// |   / /_  / ___/ __ `| |/_/  / /_  / / __ \/ __ `/ __ \/ ___/ _ \  |
+// |  / __/ / /  / /_/ _>  <   / __/ / / / / / /_/ / / / / /__/  __/  |
+// | /_/   /_/   \__,_/_/|_|  /_/   /_/_/ /_/\__,_/_/ /_/\___/\___/   |
+// |                                                                  |
+// ====================================================================
+// ======================= CrossChainCanonicalV2 ======================
+// ====================================================================
+// Cross-chain / non mainnet canonical token contract.
+// Does not include any spurious mainnet logic
+// Does have authorized minters
+
+// Frax Finance: https://github.com/FraxFinance
+
+// Primary Author(s)
+// Travis Moore: https://github.com/FortisFortuna
+// Jack Corddry: https://github.com/corddry
+
+// Reviewer(s) / Contributor(s)
+// Sam Kazemian: https://github.com/samkazemian
+// Dennis: https://github.com/denett
+
+import { ERC20PermitPermissionedMint } from "../ERC20PermitPermissionedMint.sol";
+
+contract CrossChainCanonicalV2 is ERC20PermitPermissionedMint {
+    /* ========== CONSTRUCTOR ========== */
+    constructor(
+        address _creator_address,
+        address _timelock_address,
+        string memory _name,
+        string memory _symbol
+    ) ERC20PermitPermissionedMint(_creator_address, _timelock_address, _name,  _symbol) 
+    {
+
+    }
+}
