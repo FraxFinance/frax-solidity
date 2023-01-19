0724df110d9dd889331f7eb7ec7466e88be5e4e9
ZrowGz <zrowgz@gmail.com>
2023-01-19 13:22:27 -0800

    file sync

--- src/hardhat/contracts/Misc_AMOs/olympus/IStakingHelper.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
-pragma solidity >=0.6.11;
-
-interface IStakingHelper {
-  function OHM() external view returns (address);
-  function stake(uint256 _amount) external;
-  function staking() external view returns (address);
-}
