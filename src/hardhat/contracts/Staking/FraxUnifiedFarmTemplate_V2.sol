240acd49758afbd2eca9f15d84ad2f27cb43f6e1
ZrowGz <zrowgz@gmail.com>
2023-01-19 13:28:02 -0800

    file sync

--- src/hardhat/contracts/Staking/FraxUnifiedFarmTemplate_V2.sol
+++ src/hardhat/contracts/Staking/FraxUnifiedFarmTemplate_V2.sol
@@ -81,7 +81,7 @@ contract FraxUnifiedFarmTemplate_V2 is OwnedV2, ReentrancyGuardV2 {
 
     // Lock time and multiplier settings
     uint256 public lock_max_multiplier = 2e18; // E18. 1x = e18
-    uint256 public lock_time_for_max_multiplier = 1 * 1095 * 86400; // 3 years
+    uint256 public lock_time_for_max_multiplier = 1 * 365 * 86400; // 1 years
     // uint256 public lock_time_for_max_multiplier = 2 * 86400; // 2 days
     uint256 public lock_time_min = 594000; // 6.875 * 86400 (~7 day)
 
